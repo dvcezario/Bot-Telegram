5217238513fdf79c975d6a05a5d63ae48d8a3537
Quintans <vitor.isilva@sptech.school>
2024-04-24 12:56:34 -0300

    TUDO FUNCIONANDO CORRETAMENTE, FALTANDO APENAS QUANDO O USUÁRIO DIGITAR..

--- menuInformacoes.js
+++ menuInformacoes.js
@@ -2,11 +2,13 @@
 
 const { Markup } = require('telegraf');
 const { mensagensIDS } = require('./telaInicial');
+const path = require('path');
 
 // Função para enviar o vídeo explicativo
 async function enviarVideoExplicativo(ctx) {
     // Envie o vídeo explicativo para o usuário (substitua o URL pelo URL do vídeo)
-    const salvarId = await ctx.replyWithVideo({ source: 'https://example.com/video_explicativo.mp4' });
+    const videoExplicativoPath = path.join(__dirname, 'VideoExplicativo.mp4');
+    const salvarId = await ctx.replyWithVideo({ source: videoExplicativoPath });
     if (salvarId) {
         mensagensIDS.push(salvarId.message_id);
     }
@@ -17,7 +19,8 @@ async function enviarVideoExplicativo(ctx) {
 // Função para enviar o texto explicativo
 async function enviarTextoExplicativo(ctx) {
     // Envie o texto explicativo para o usuário (substitua o texto pelo seu texto explicativo)
-    const salvarId = await ctx.reply('Aqui está o texto explicativo sobre o jogo...');
+    const textoExplicativoPath = path.join(__dirname, 'TextoExplicativo.docx');
+    const salvarId = await ctx.replyWithDocument({ source: textoExplicativoPath });
     if (salvarId) {
         mensagensIDS.push(salvarId.message_id);
     }
@@ -27,7 +30,8 @@ async function enviarTextoExplicativo(ctx) {
 // Função para enviar informações sobre pagamento
 async function enviarInformacoesPagamento(ctx) {
     // Envie as informações sobre pagamento para o usuário (substitua o texto pelo seu texto sobre pagamento)
-    const salvarId = await ctx.reply('Aqui estão as informações sobre pagamento...');
+    const pagamentoJogoPath = path.join(__dirname, 'PagamentoJogo.docx');
+    const salvarId = await ctx.replyWithDocument({ source: pagamentoJogoPath });
     if (salvarId) {
         mensagensIDS.push(salvarId.message_id);
     }
@@ -37,7 +41,8 @@ async function enviarInformacoesPagamento(ctx) {
 // Função para enviar informações sobre recebimento
 async function enviarInformacoesRecebimento(ctx) {
     // Envie as informações sobre recebimento para o usuário (substitua o texto pelo seu texto sobre recebimento)
-    const salvarId = await ctx.reply('Aqui estão as informações sobre recebimento...');
+    const recebimentoPremioPath = path.join(__dirname, 'RecebimentoPremio.docx');
+    const salvarId = await ctx.replyWithDocument({ source: recebimentoPremioPath });
     if (salvarId) {
         mensagensIDS.push(salvarId.message_id);
     }
